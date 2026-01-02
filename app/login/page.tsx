b8d104f317c6108a7448888ad3e08769bb5e109b
Yash <yashgole2911@gmail.com>
2026-01-02 14:22:38 +0530

    login UI update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,17 @@
-export default function Login() {
-  return (
+
+    export default function Login() {
+        return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Login</h1>
+
+      <div className="flex flex-col items-center mb-6">
+  <h1 className="text-3xl font-bold text-blue-500">
+    Home/mate
+  </h1>
+  <p className="text-black text-lg mt-1">
+    Welcome!
+  </p>
+</div>
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
       <form className="flex flex-col gap-3">
         <input type="email" placeholder="Email" className="border p-2 rounded"/>
         <input type="password" placeholder="Password" className="border p-2 rounded"/>
