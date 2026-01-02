bfaa22c9238c94c014fa746605a0094046041b20
Yash <yashgole2911@gmail.com>
2026-01-02 15:33:53 +0530

    sign in UI update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,46 @@
-export default function register() {
+export default function Register() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Register</h1>
-      <form className="flex flex-col gap-3">
-        <input type="email" placeholder="Email" className="border p-2 rounded"/>
-        <input type="password" placeholder="Password" className="border p-2 rounded"/>
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded">Register</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-80 flex flex-col gap-4">
+
+        <h1 className="text-2xl font-bold text-center">Sign Up</h1>
+        <p className="text-sm text-grey-500 text-center">
+          Let's create a new account to get started
+        </p>
+        <p>Full name*</p>
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="border p-2 rounded"
+        />
+        <p>Mobile No.*</p>
+        <input
+          type="text"
+          placeholder="Mobile No."
+          className="border p-2 rounded"
+        />
+         <p>Email Id</p>
+        <input
+          type="email"
+          placeholder="Email id"
+          className="border p-2 rounded"
+        />
+        <p>Location</p>
+        <input
+          type="text"
+          placeholder="Location"
+          className="border p-2 rounded"
+        />
+        <form>
+        <div className="flex justify-center mt-4">
+  <button className="bg-blue-500 text-white px-6 py-2 rounded-md">
+    Sign up
+  </button>
+</div>
+</form>
+        
+
+      </div>
     </div>
   );
 }
